84c8437bc4df7a43f4bda7a5385ef2a0cac3b0fd
David Logie <d@djl.im>
2011-11-15 09:34:31 +0000

    Fix growlnotify links.

--- growlnotify/growlnotify.rb
+++ growlnotify/growlnotify.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Growlnotify < Formula
   if RUBY_PLATFORM =~ /darwin11/
-    url 'https://s3.amazonaws.com/djl.etc/growlnotify-v1.3.tar.gz'
+    url 'https://s3.amazonaws.com/djl.homebrew/growlnotify-v1.3.tar.gz'
     md5 '44f771b0bafd0fc077ecc546841d9831'
   else
-    url 'https://s3.amazonaws.com/djl.etc/growlnotify-v1.2.2.tar.gz'
+    url 'https://s3.amazonaws.com/djl.homebrew/growlnotify-v1.2.2.tar.gz'
     md5 'cfe9a988a0a64aba128baf363050b434'
   end
 
