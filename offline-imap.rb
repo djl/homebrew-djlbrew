a161332ff175afe4c7d586b363be02b037858ba5
David Logie <d@djl.im>
2012-05-12 12:45:48 +0100

    Back to regular offline-imap releases.

--- offline-imap.rb
+++ offline-imap.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class OfflineImap < Formula
-  url "https://github.com/spaetz/offlineimap.git", :branch => 'next'
-  version 'HEAD'
-  homepage "http://offlineimap.org/"
+  homepage 'http://offlineimap.org/'
+  url 'https://github.com/spaetz/offlineimap/tarball/v6.5.3.1'
+  md5 '40a631fe164c74bb92933a1e683a10a1'
 
   def install
     libexec.install 'bin/offlineimap' => 'offlineimap.py'
