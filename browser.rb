64ea1c0f1bfd86ffce93a00acbf9e69643f51d40
David Logie <d@djl.im>
2012-11-20 14:58:52 +0000

    Remove browser formula.

--- browser.rb
+++
@@ -1,7 +0,0 @@
-require 'formula'
-
-class Browser < GithubGistFormula
-  url 'https://raw.github.com/gist/3969783/78fbd60f94d0ecd2847154547101f63748aad3e7/browser'
-  homepage 'https://gist.github.com/3969783'
-  sha1 'a78fb4549c2614d8df1117e2eb206a724579f5e6'
-end
