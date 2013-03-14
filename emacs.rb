294e11ee0df69d15af2dcb3d12a3fb598e5a1b06
David Logie <d@djl.im>
2013-03-14 15:28:30 +0000

    Even simpler emacs formula.

--- emacs.rb
+++ emacs.rb
@@ -6,39 +6,15 @@ class Emacs < Formula
   mirror 'http://ftp.gnu.org/pub/gnu/emacs/emacs-24.3.tar.gz'
   sha256 '0098ca3204813d69cd8412045ba33e8701fa2062f4bff56bedafc064979eef41'
 
-  option "srgb", "Enable sRGB colors in the Cocoa version of emacs"
-  option "use-git-head", "Use Savannah git mirror for HEAD builds"
-
-  head 'bzr://http://bzr.savannah.gnu.org/r/emacs/trunk'
-
   depends_on :autoconf
   depends_on :automake
 
-  fails_with :llvm do
-    build 2334
-    cause "Duplicate symbol errors while linking."
-  end
-
   def install
-    # HEAD builds are currently blowing up when built in parallel
-    # as of April 20 2012
-    ENV.j1 if build.head?
-
-    # See: https://github.com/mxcl/homebrew/issues/4852
-    if build.head? and File.exists? "./autogen/copy_autogen"
-      system "autogen/copy_autogen"
-    end
-
-    # Patch for color issues described here:
-    # http://debbugs.gnu.org/cgi/bugreport.cgi?bug=8402
-    if build.include? "srgb"
-      inreplace "src/nsterm.m",
-        "*col = [NSColor colorWithCalibratedRed: r green: g blue: b alpha: 1.0];",
-        "*col = [NSColor colorWithDeviceRed: r green: g blue: b alpha: 1.0];"
-    end
-
+     inreplace "src/nsterm.m",
+       "*col = [NSColor colorWithCalibratedRed: r green: g blue: b alpha: 1.0];",
+       "*col = [NSColor colorWithDeviceRed: r green: g blue: b alpha: 1.0];"
     system "./configure", "--with-ns"
-    system "make clean"
+    system "make bootstrap"
     system "make install"
     prefix.install "nextstep/Emacs.app"
   end
