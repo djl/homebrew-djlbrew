3c8b6c2f4ae437da21f4dcc8c5f5874e9f5ea11a
David Logie <d@djl.im>
2013-06-23 16:01:47 +0100

    Bump darcs to 2.8.4.

--- darcs.rb
+++ darcs.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Darcs < Formula
   homepage 'http://darcs.net/'
-  url 'http://darcs.net/binaries/macosx/darcs-2.5-OSX-10.6-i386.tar.gz'
-  version '2.5'
-  sha1 'b7c10929952d06c3d3a38dd7ca166b0742ad3706'
+  url 'http://darcs.net/binaries/macosx/darcs-2.8.4-OSX-10.8.4-x86_64.tar.bz2'
+  version '2.8.4'
+  sha1 'c89fb8cf68ef7f9f61e3ad1a7514236c2cdb437d'
 
   def install
     bin.install "darcs"
