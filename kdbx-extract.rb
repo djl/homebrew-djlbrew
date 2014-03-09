bb6123c1351b6e1a143b9efcf0b83cac21bc42f1
David Logie <d@djl.im>
2014-03-09 13:22:59 +0000

    Update kdbx-extract URL and add caveats.

--- kdbx-extract.rb
+++ kdbx-extract.rb
@@ -2,12 +2,19 @@ require 'formula'
 
 class KdbxExtract < Formula
   url 'https://s3.amazonaws.com/djl.homebrew/kdbx-extract-2.0-alpha5'
-  homepage 'http://www.keepassx.org/'
+  homepage 'https://github.com/djl/keepassx/tree/kdbx-extract-prompt'
   version '2.0-alpha5'
   sha1 '6f5c5628dcae57581eddbf9e18e6efa5aa35ce5f'
 
   def install
     bin.install 'kdbx-extract-2.0-alpha5' => 'kdbx-extract'
+  end
+
+  def caveats; <<-EOS.undent
+    This is a custom built version of the kdbx-extract utility. It
+    will prompt for a passphrase rather than require one be given
+    on the command line.
+    EOS
   end
 end
 
