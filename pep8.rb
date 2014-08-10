23e6efc75ccb38dec011b64168513ccb57b933b4
David Logie <d@djl.io>
2014-08-10 12:03:43 +0100

    Bump pep8.

--- pep8.rb
+++ pep8.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Pep8 < Formula
-  url 'https://github.com/jcrocholl/pep8/archive/1.4.6.tar.gz'
+  url 'https://github.com/jcrocholl/pep8/archive/1.5.7.tar.gz'
   homepage 'https://github.com/jcrocholl/pep8/'
-  sha1 '11c34672a2f5df94d1f1da3863e1aebda4feaa91'
+  sha1 '1687e5e28bdd123cc37614ab3c444fc821aec726'
 
   def install
     bin.install 'pep8.py' => 'pep8'
