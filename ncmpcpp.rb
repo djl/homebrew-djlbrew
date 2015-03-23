153be697b351abbd2ca00a2f5c818349b4f60130
David Logie <djl@mksh.org>
2015-03-23 15:57:01 +0000

    Add custom ncmpcpp recipe.

---
+++ ncmpcpp.rb
@@ -0,0 +1,56 @@
+class Ncmpcpp < Formula
+  homepage "http://ncmpcpp.rybczak.net/"
+  url "http://ncmpcpp.rybczak.net/stable/ncmpcpp-0.6.3.tar.bz2"
+  sha1 "dbc121b766791eae0a1613f29131adcc36c39065"
+
+  head do
+    url "git://repo.or.cz/ncmpcpp.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libmpdclient"
+  depends_on "readline"
+
+  if MacOS.version < :mavericks
+    depends_on "boost" => "c++11"
+    depends_on "taglib" => "c++11"
+  else
+    depends_on "boost"
+    depends_on "taglib"
+  end
+
+  depends_on "fftw" if build.include? "visualizer"
+
+  option "outputs", "Compile with mpd outputs control"
+  option "visualizer", "Compile with built-in visualizer"
+  option "clock", "Compile with optional clock tab"
+
+  needs :cxx11
+
+  def install
+    ENV.cxx11
+    ENV.append "LDFLAGS", "-liconv"
+
+    args = ["--disable-dependency-tracking",
+            "--prefix=#{prefix}",
+            "--with-taglib",
+            "--with-curl",
+            "--enable-unicode"]
+
+    args << "--enable-outputs" if build.include? "outputs"
+    args << "--enable-visualizer" if build.include? "visualizer"
+    args << "--enable-clock" if build.include? "clock"
+
+    if build.head?
+      # Also runs configure
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
+    system "make", "install"
+  end
+end
