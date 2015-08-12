8da431e92720b88ceed4547d2d79c17508a84a0e
David Logie <djl@mksh.org>
2015-08-12 11:53:00 +0100

    openssh: bump to 7.0p1.

--- openssh.rb
+++ openssh.rb
@@ -1,29 +1,57 @@
-require 'formula'
-
 class Openssh < Formula
-  homepage 'http://www.openssh.com/'
-  url 'http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.8p1.tar.gz'
-  version '6.8p1'
-  sha256 '3ff64ce73ee124480b5bf767b9830d7d3c03bbcb6abe716b78f0192c37ce160e'
+  desc "OpenBSD freely-licensed SSH connectivity tools"
+  homepage "http://www.openssh.com/"
+  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.0p1.tar.gz"
+  mirror "https://www.mirrorservice.org/pub/OpenBSD/OpenSSH/portable/openssh-7.0p1.tar.gz"
+  version "7.0p1"
+  sha256 "fd5932493a19f4c81153d812ee4e042b49bbd3b759ab3d9344abecc2bc1485e5"
 
-  depends_on 'openssl'
+  # Please don't resubmit the keychain patch option. It will never be accepted.
+  # https://github.com/Homebrew/homebrew-dupes/pull/482#issuecomment-118994372
+  option "with-libressl", "Build with LibreSSL instead of OpenSSL"
+
+  depends_on "openssl" => :recommended
+  depends_on "libressl" => :optional
+  depends_on "ldns" => :optional
+  depends_on "pkg-config" => :build if build.with? "ldns"
 
   patch :p0 do
-    url 'https://raw.githubusercontent.com/voidlinux/void-packages/57703a9c08620b4e680439902db5ad797bd47785/srcpkgs/openssh/files/openssh-6.8p1-identitypersist.patch'
+    url 'https://raw.githubusercontent.com/voidlinux/void-packages/57703a9c08620b4e680439902db5ad797bd47785/srcpkgs/openssh/files/openssh-#{version}-identitypersist.patch'
     sha256 '0382e2c8a82e9363dc2eed0150d94f5207d9f71de01f5691717d7a5976f32f5a'
   end
 
+  patch do
+    url "https://gist.githubusercontent.com/jacknagel/e4d68a979dca7f968bdb/raw/f07f00f9d5e4eafcba42cc0be44a47b6e1a8dd2a/sandbox.diff"
+    sha256 "82c287053eed12ce064f0b180eac2ae995a2b97c6cc38ad1bdd7626016204205"
+  end
+
+  # Patch for SSH tunnelling issues caused by launchd changes on Yosemite
+  patch do
+    url "https://trac.macports.org/export/138238/trunk/dports/net/openssh/files/launchd.patch"
+    sha256 "012ee24bf0265dedd5bfd2745cf8262c3240a6d70edcd555e5b35f99ed070590"
+  end
+
   def install
-    args = [
-      "--with-libedit",
-      "--with-kerberos5",
-      "--prefix=#{prefix}",
-      "--sysconfdir=#{etc}/ssh",
-      "--with-ssl-dir=#{Formula["openssl"].opt_prefix}",
+    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
+
+    args = %W[
+      --with-libedit
+      --with-pam
+      --with-kerberos5
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}/ssh
     ]
+
+    if build.with? "libressl"
+      args << "--with-ssl-dir=#{Formula["libressl"].opt_prefix}"
+    else
+      args << "--with-ssl-dir=#{Formula["openssl"].opt_prefix}"
+    end
+
+    args << "--with-ldns" if build.with? "ldns"
 
     system "./configure", *args
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
