31f9ce67db602aab40a7eed9fb503e6e7dcf1bd0
David Logie <djl@mksh.org>
2016-03-01 15:34:54 +0000

    openssh: Update to 7.2p1.
    With the new AddKeysToAgent feature this formula can be removed
    entirely when homebrew-dupes adds 7.2p1.

--- openssh.rb
+++ openssh.rb
@@ -1,13 +1,11 @@
 class Openssh < Formula
   desc "OpenBSD freely-licensed SSH connectivity tools"
   homepage "http://www.openssh.com/"
-  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.1p2.tar.gz"
-  mirror "https://www.mirrorservice.org/pub/OpenBSD/OpenSSH/portable/openssh-7.1p2.tar.gz"
-  version "7.1p2"
-  sha256 "dd75f024dcf21e06a0d6421d582690bf987a1f6323e32ad6619392f3bfde6bbd"
+  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.2p1.tar.gz"
+  mirror "https://www.mirrorservice.org/pub/OpenBSD/OpenSSH/portable/openssh-7.2p1.tar.gz"
+  version "7.2p1"
+  sha256 "973cc37b2f3597e4cf599b09e604e79c0fe5d9b6f595a24e91ed0662860b4ac3"
 
-  # Please don't resubmit the keychain patch option. It will never be accepted.
-  # https://github.com/Homebrew/homebrew-dupes/pull/482#issuecomment-118994372
   option "with-libressl", "Build with LibreSSL instead of OpenSSL"
 
   depends_on "openssl" => :recommended
@@ -15,25 +13,7 @@ class Openssh < Formula
   depends_on "ldns" => :optional
   depends_on "pkg-config" => :build if build.with? "ldns"
 
-  patch :p0 do
-    url 'https://raw.githubusercontent.com/voidlinux/void-packages/877f46fba99fa539bc9ac9cd3aca6a44c682bfad/srcpkgs/openssh/files/openssh-6.8p1-identitypersist.patch'
-    sha256 '0382e2c8a82e9363dc2eed0150d94f5207d9f71de01f5691717d7a5976f32f5a'
-  end
-
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a74/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
-    sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
-  end
-
-  # Patch for SSH tunnelling issues caused by launchd changes on Yosemite
-  patch do
-    url "https://trac.macports.org/export/138238/trunk/dports/net/openssh/files/launchd.patch"
-    sha256 "012ee24bf0265dedd5bfd2745cf8262c3240a6d70edcd555e5b35f99ed070590"
-  end
-
   def install
-    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
-
     args = %W[
       --with-libedit
       --with-pam
