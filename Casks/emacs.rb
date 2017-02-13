d8db446261f9785d51a1099171dc226f43e84c86
David Logie <djl@mksh.org>
2017-02-13 12:52:52 +0000

    Remove Casks.

--- Casks/emacs.rb
+++
@@ -1,9 +0,0 @@
-cask 'emacs' do
-  name 'Emacs'
-  homepage 'https://emacsformacosx.com/'
-  version '25.1'
-  url "https://emacsformacosx.com/emacs-builds/Emacs-#{version}-universal.dmg"
-  sha256 '5bd04c82d2a091a48d7b9b80a26fa31f64767f2e553440a556d12be5815e29ef'
-  license :gpl
-  app 'Emacs.app'
-end
